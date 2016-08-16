394fcd839174408a721ef785ec300936d97fe953
BlackMambaCoder <leo.elfak@gmail.com>
2016-08-16 21:19:09 +0200

    Location update service

---
+++ DriveToTravel/app/src/main/java/rs/elfak/mosis/drivetotravel/drivetotravel1/Exception/HttpConnectionException.java
@@ -0,0 +1,10 @@
+package rs.elfak.mosis.drivetotravel.drivetotravel1.Exception;
+
+/**
+ * Created by LEO on 13.8.2016..
+ */
+public class HttpConnectionException extends Exception {
+    public HttpConnectionException (String message){
+        super(message);
+    }
+}
